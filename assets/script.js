1a9ad03dcd990a057ade5e46dce04bb4e34453b6
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2024-03-16 16:06:53 -0300

    organizando

--- assets/script.js
+++ assets/script.js
@@ -10,20 +10,19 @@ function mostraHora() {
   const msg = document.querySelector("#msg");
   const imagem = document.querySelector("#imagem");
   const data = gerarData()
-  //const hora = gerarHora(data)
-  const hora = 
+  const hora = gerarHora(data)
 
   msg.innerHTML = `Agora são ${hora} horas`;
   if (hora >= 0 && hora <= 12) {
-    imagem.src = "fotomanha.jpg";
+    imagem.src = "/assets/fotomanha.jpg";
     titulo.innerHTML = '<strong>Bom dia!</strong>'
     document.body.style.backgroundColor = '#eef115'
   } else if (hora > 12 && hora < 18){
-    imagem.src = 'fototarde.jpg'
+    imagem.src = '/assets/fototarde.jpg'
     titulo.innerHTML = '<strong>Boa tarde!</strong>'
     document.body.style.backgroundColor = '#eeba10'
   } else {
-    imagem.src = 'fotonoite.jpg'
+    imagem.src = '/assets/fotonoite.jpg'
     titulo.innerHTML = '<strong>Boa noite!</strong>'
     document.body.style.backgroundColor = '#1b1620'
   }
